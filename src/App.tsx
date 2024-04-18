e1da96e11a4a6df1276d3f107bb45478fbcbcc72
funayamateppei <t.funayama0217@gmail.com>
2024-04-18 12:57:21 +0900

    storeをネストした状態で動作するかの検証

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,81 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { createStore } from "zustand";
+import { createStore, StoreApi, useStore } from "zustand";
+import { useMemo, useState } from "react";
 
-type State = {
-    count: number;
-    increaseCount: () => void;
-    resetCount: () => void;
-    decreaseCount: () => void;
-};
-const useCountStore = createStore<State>((set) => ({
-    count: 0,
-    increaseCount: () => set((state) => ({ count: state.count + 1 })),
-    decreaseCount: () => set((state) => ({ count: state.count - 1 })),
-    resetCount: () => set({ count: 0 }),
-}));
-
-function CounterDisplay() {
-    const count = useCountStore.getState().count;
-    return <p>{count}</p>;
+interface DemoStore {
+    data: string;
+    setData: (newData: string) => void;
 }
 
-function Counter() {
-    const increase = useCountStore.getState().increaseCount;
-    const decrease = useCountStore.getState().decreaseCount;
-    const reset = useCountStore.getState().resetCount;
+interface CountStore {
+    count: number;
+    increaseCount: () => void;
+    decreaseCount: () => void;
+    resetCount: () => void;
+}
 
-    useCountStore.subscribe(({ count }) => {
-        console.log(count);
-    });
+interface State {
+    demoStore: StoreApi<DemoStore>;
+    countStore: StoreApi<CountStore>;
+}
+
+const useStateStore = createStore<State>(() => ({
+    demoStore: createStore<DemoStore>((set) => ({
+        data: "demo",
+        setData: (newData: string) => set({ data: newData }),
+    })),
+
+    countStore: createStore<CountStore>((set) => ({
+        count: 0,
+        increaseCount: () => set((state) => ({ count: state.count + 1 })),
+        decreaseCount: () => set((state) => ({ count: state.count - 1 })),
+        resetCount: () => set({ count: 0 }),
+    })),
+}));
+
+function Counter() {
+    const countStore = useStore(useStateStore, (state) => state.countStore);
+
+    const count = useStore(countStore, (state) => state.count);
+    const increase = useStore(countStore, (state) => state.increaseCount);
+    const decrease = useStore(countStore, (state) => state.decreaseCount);
+    const reset = useStore(countStore, (state) => state.resetCount);
+
+    const countTypography = useMemo(() => <p>{count}</p>, [count]);
 
     return (
         <div className="card">
             <button onClick={increase}>Count Up</button>
             <button onClick={decrease}>Count down</button>
             <button onClick={reset}>Reset</button>
+
+            {countTypography}
+        </div>
+    );
+}
+
+function DemoData() {
+    const [string, setString] = useState<string>("");
+    const demoStore = useStore(useStateStore, (state) => state.demoStore);
+
+    const data = useStore(demoStore, (state) => state.data);
+    const setData = useStore(demoStore, (state) => state.setData);
+
+    return (
+        <div className="card">
+            <input value={string} onChange={(e) => setString(e.target.value)} />
+            <button
+                onClick={() => {
+                    setString("");
+                    setData(string);
+                }}
+            >
+                更新
+            </button>
+
+            <p>{data}</p>
         </div>
     );
 }
@@ -54,9 +95,9 @@ function App() {
 
             <h1>Vite + React</h1>
 
-            <CounterDisplay />
-
             <Counter />
+
+            <DemoData />
         </>
     );
 }
